f555e51a309984666f3e32007478b1f0cb92d3ff
BigLazyET <xjl505302554@outlook.com>
2026-09-04 14:40:16 +0800

    opentelemetry

--- mysql-cdc/mysql-init/01-replication-user.sql
+++ mysql-cdc/mysql-init/01-replication-user.sql
@@ -1,3 +1,6 @@
+-- 创建一个专门用于读取 binlog/replication 的用户。
+-- '%' 表示允许从任意主机连接。因为 .NET 程序可能运行在宿主机、其他容器或其他网络位置。
+
 CREATE USER IF NOT EXISTS 'debezium'@'%' IDENTIFIED BY 'debezium_password';
 
 GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT, LOCK TABLES
